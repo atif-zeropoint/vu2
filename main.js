1ca4eaa2748e7063ddcebd5ded0a38eb7e32db4f
Atif Ali <atif@monotote.com>
2019-09-02 18:32:27 +0500

    ep 8 done

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+Vue.component('task-list', {
+    template: '<div><task v-for="task in tasks" v-text="task.description"></task></div>',
+
+    data() {
+        return {
+            tasks: [
+                {description: 'Go to Gym', completed: false},
+                {description: 'Go to the bank', completed: false},
+                {description: 'Go to work', completed: false},
+                ]
+        }
+    }
+})
+
+
 Vue.component('task', {
     template: '<li><slot>Foobar</slot></li>'
 })
