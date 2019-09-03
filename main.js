781e573d975d1aa6ff0feafb9a9996bd09c6b34e
Atif Ali <atif@monotote.com>
2019-09-03 13:21:52 +0500

    ep 10 done

--- main.js
+++ main.js
@@ -33,12 +33,28 @@ Vue.component('message', {
 });
 
 
+Vue.component('modal', {
+    template: `<div class="modal is-active" >
+                    <div class="modal-background"></div>
+                    <div class="modal-content">
+                       <div class="box">
+                            <slot></slot>      
+                       </div>
+                    </div>
+                    <button class="modal-close is-large" aria-label="close" @click="$emit(\'close\')"></button>
+                </div>
+            </div>`,
+});
+
 Vue.component('task', {
     template: '<li><slot>Foobar</slot></li>'
 })
 
 var app = new Vue({
     el: '#root',
+    data: {
+        showModal: false
+    }
 });
 
 
